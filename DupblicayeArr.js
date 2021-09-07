3a7470e85df953d766226494e4d3d3f897ef116e
AmmukhanAk <amerkhanak007@gmail.com>
2021-09-07 09:25:49 -0700

    uygyf
    yfuyy

---
+++ DupblicayeArr.js
@@ -0,0 +1,17 @@
+// const arry=[1,2,1];
+
+// const toFindDuplicates=arry=>arry.filter((item,index)=>arry.indexOfitem(!==)index
+// const duplicateElement=toFindDuplicatearry(arry);
+// console.log(duplicateElements);
+
+
+const array=[1, 2, 4, 2, 5,];
+const newArr=[];
+array.forEach((element, index) => {
+    array.indexOf(element)==index && newArr.push(element)
+});
+console.log(newArr);
+        
+        
+    
+
---
+++ index.js
@@ -0,0 +1,15 @@
+// let name = "Ammukhan";
+
+// console.log(name);
+
+
+
+// let firstName ="Amer "
+// let lastName="khan"
+
+// console.log(firstName+lastName)
+
+
+let interestRate=0.3
+interestRate =1
+console.log(interestRate)
